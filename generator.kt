0d676857fd3cbdb04d97c339f9287afcff6c3782
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-05-23 15:50:34 +0300

    Create generator.kt

---
+++ generator.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val jokes = listOf(
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        "Did you hear about the mathematician who’s afraid of negative numbers? He will stop at nothing to avoid them!",
+        "Why don't skeletons fight each other? They don't have the guts!",
+        "I told my wife she should embrace her mistakes. She gave me a hug.",
+        "I was addicted to the hokey pokey, but then I turned myself around.",
+        "Why did the bicycle fall over? Because it was two-tired!"
+        "I'm so good at coding some stuff."
+        "You're so beautiful."
+        "Is it joke?"
+    )
+
+    val randomJoke = jokes.random()
+    println(randomJoke)
+}
